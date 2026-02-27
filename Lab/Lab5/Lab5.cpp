dc88553c6b8357ce306f2549484b5ae8e62c7f20
t1sun1012 <t1sun@outlook.com>
2026-02-27 16:24:42 -0500

    lab5 finish

--- Lab/Lab5/Lab5.cpp
+++ Lab/Lab5/Lab5.cpp
@@ -12,7 +12,7 @@ void reverse_inplace(char* str);
 char* reverse(char* str);
 
 
-/*
+
 
 int main() {
     // test for the functions
@@ -27,10 +27,12 @@ int main() {
     reverse_inplace(ptest);
     cout << test << endl;
 
-    
-    cout << reverse(ptest) << endl;
+    char* r = reverse(ptest);
+    cout << r << endl;
+    delete[] r;
+
+    return 0;
 }
-*/
 
 
 
@@ -56,14 +58,15 @@ void reverse_inplace(char* str) {
 
 char* reverse(char* str) {
     // real length is without the null \0
-    int length = word_length(str) - 1;
+    int length = word_length(str);
 
-    //new string on the heap to store the result, same length as original string
-    char *result = new char[length]; 
+    //new string on the heap to store the result, same length as original string but +1 to include \0
+    char *result = new char[length + 1]; 
 
     
-    for (int i = 0, j = length; i < (length +1); i++, j--) {
-        result[j] = str[i];
+    for (int i = 0; i < length; i++) {
+        result[length -1 -i] = str[i];
     }
+    result[length] = '\0';
     return result;
 }
